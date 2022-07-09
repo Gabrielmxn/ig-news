96485343da24bf769b626c685a64215f371580be
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-07-09 10:16:26 -0300

    :rocket: adiciona feature
    Foi criado uma pasta de rota de API para autenticação com GITHUB, com next

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,17 +1,20 @@
 import { FaGithub } from "react-icons/fa";
 import styles from './styles.module.scss';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
+  const {data: session} = useSession();
+  console.log(session)
 
-  return isUserLoggedIn ? (
+  return session ? (
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="#04d3e1"/>
-      Gabriel Alves
+      {session.user.name}
       
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
@@ -20,6 +23,7 @@ export function SignInButton(){
     <button 
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417"/>
       Sign in with Github
