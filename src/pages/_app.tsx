f49c4dc2713d9e1b3d8155fa659882491a412a8e
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-07-18 22:44:01 -0300

    :sparkles: Adiciona o PrismicProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,31 @@ import type { AppProps } from 'next/app'
 import { Header } from '../components/Header';
 import { SessionProvider  as NextAuthProvider } from 'next-auth/react';
 import '../styles/global.scss';
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { linkResolver, repositoryName } from '../services/prismic'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
-    <NextAuthProvider session={pageProps.session}>
-    <Header />
-     <Component {...pageProps} />
-    </NextAuthProvider>
+    <PrismicProvider
+    linkResolver={linkResolver}
+    internalLinkComponent={({ href, children, ...props }) => (
+      <Link href={href}>
+        <a {...props}>
+          {children}
+        </a>
+      </Link>
+    )}
+    >
+      <NextAuthProvider session={pageProps.session}>
+        <Header />
+        <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+      </NextAuthProvider>
+    </PrismicProvider>
+   
   )
 }
 
