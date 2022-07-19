29bd2caa59b29a0124f8d269213592743816628d
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2022-07-18 22:43:25 -0300

    :sparkles: Mudança de rota no menu

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,27 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
 
+interface TypePageSelect{
+  home: boolean;
+  post: boolean;
+}
 export function Header(){
+  const router = useRouter()
+  
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Logo" />
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Post</a>
+          <Link href="/">
+            <a className={router.asPath === '/' ? styles.active : ''}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a className={router.asPath === '/posts' ? styles.active : ''}>Post</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
